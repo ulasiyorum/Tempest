70dd8a3638ecccc4c27d7fe0bb1c797b6a970baf
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-01-01 21:23:35 +0300

    add: collectable class

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable
+{
+    private int id; // To tell us what type of collectable is this
+    private float x; // position on current map
+    private float y; // position on current map
+}
